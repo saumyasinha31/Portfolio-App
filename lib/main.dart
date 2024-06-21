f09772668f9635e04883ac947eb63526726f76b0
SAUMYA SINHA <saumyasinha269@gmail.com>
2024-06-21 15:37:19 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
@@ -12,62 +12,135 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.only(top:100.0, left: 20),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  CircleAvatar(radius: 40,),
-                  Column(
-                    children: [
-                      Text("Name"),
-                      Text('Designation')
-                    ],
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Icon(Icons.school_rounded),
-                      Text("School Name"),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.school_rounded),
-                      Text("School Name"),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.school_rounded),
-                      Text("School Name"),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.school_rounded),
-                      Text("School Name"),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.school_rounded),
-                      Text("School Name"),
-                    ],
-                  ),
-
-                ],
-              ),
-              Text('About me'),
-              Text('Created by'),
-            ],
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/bakgroundimages.jpeg"),
+              fit: BoxFit.cover,
+            ),
           ),
-        )
+          child: Padding(
+                padding: const EdgeInsets.only(top: 100.0, left: 20),
+                child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage("images/image.jpeg"),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Saumya Sinha", style: TextStyle(fontSize: 30,fontFamily: 'Alvina')),
+                    Text('App Developer', style: TextStyle(fontSize: 25))
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.school_rounded,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Text("KIET Group of Institutions", style: TextStyle(fontSize: 20,fontFamily: 'Alvina')),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.computer_rounded,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Text("Projects", style: TextStyle(fontSize: 20,fontFamily: 'Alvina')),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_on_outlined,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Text("Ghaziabad", style: TextStyle(fontSize: 20 , fontFamily: 'Alvina')),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.email_outlined,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Text("saumyasinha269@gmail.com", style: TextStyle(fontSize: 20,fontFamily: 'Alvina')),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.person_outlined,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Text("Positions of Responsibility",
+                          style: TextStyle(fontSize: 20,fontFamily: 'Alvina')),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Text("Achievements", style: TextStyle(fontSize: 20,fontFamily: 'Alvina')),
+                    ],
+                  ),
 
-    );
+                ],
+              ),
+            ),
+            // SizedBox(height: 10,),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text('I am a versatile professional with expertise in mobile app development, AR/VR development, and UI/UX design. My diverse skill set allows me to create immersive and user-friendly digital experiences across various platforms.'),
+            ),
+            // SizedBox(height: 10,),
+            Text('Created by Saumya Sinha'),
+          ],
+                ),
+              ),
+        ));
   }
 }
